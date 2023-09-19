a966f657a7cf4a420af15beed3bc4243f4020810
Dmitry <a1@macbook-air-1.local>
2023-09-19 19:16:11 +0300

    feat: add navigation menu, cv foto and added style day and night theme '(Th, Sep 19, 2023 19:16:00)'

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,8 @@
-console.log('hello')
+const changeThemBtn = document.querySelector('.change__them_btn');
+const body = document.querySelector('.body');
+
+const addClass = () => {
+    body.classList.toggle('day-them')
+};
+
+changeThemBtn.addEventListener('click', addClass);
